ad6241c247ccbaff70e328ee028a20bd58a6e964
Wojciech Kosierkiewicz <wojtekkosierkiewicz@gmail.com>
2025-09-22 14:33:31 +0200

    Initial Commit

---
+++ Hbit/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Hbit
+//
+//  Created by Wojciech Kosierkiewicz on 22/09/2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
